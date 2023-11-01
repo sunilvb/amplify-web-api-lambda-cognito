3f75c914de81d497f42789baebbd96ed46340c0c
Vishnubhotla <sunilvbs@3c0630190b8c.ant.amazon.com>
2023-11-01 12:40:48 -0400

    updated title

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_EMB8T3oIb', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '463so9cf9k03jfb4snb42hevs0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
